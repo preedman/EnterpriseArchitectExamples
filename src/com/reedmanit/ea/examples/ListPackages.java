74a031559d0d82a8a29eb580ca75808683eb70fd
PaulReedman <preedman@gmail.com>
2019-12-08 10:35:04 +1000

    v0.3

--- src/com/reedmanit/ea/examples/ListPackages.java
+++ src/com/reedmanit/ea/examples/ListPackages.java
@@ -1,16 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Small example demonstrating how to iterate through a Sparx Model.
  */
 package com.reedmanit.ea.examples;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 import org.sparx.Collection;
 import org.sparx.Repository;
 
 /**
  *
- * @author paul
+ * @author paul reedman
  */
 public class ListPackages {
 
@@ -20,29 +22,42 @@ public class ListPackages {
     private static Repository rep;
 
     public static void main(String[] args) {
-        
+
         try {
+
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Enter file name for sparx model: ");
+            String repoLocation = input.next();
+            System.out.println("File entered = " + repoLocation);
+            
+            Path path = Paths.get(repoLocation);
+            
+            if (Files.notExists(path)) {
+                throw new Exception("File does not exist");
+            }
+
             rep = new Repository();  // get a repository
 
-            rep.OpenFile("C:\\SoftwareDevelopment\\EA-Projects\\EAExample.eap");  //open the repository file
+            
+            rep.OpenFile(repoLocation);  //open the repository file
+            
+            // Get the first Model node Iterate through all views in the model
+            
+            org.sparx.Package myModel = rep.GetModels().GetAt((short) 0);
 
-            // Get the first Model node// Iterate through all views in the model
-            org.sparx.Package myModel = rep.GetModels().GetAt( (short)0 );
-            
             Collection<org.sparx.Package> views = myModel.GetPackages();
-            
+
             for (short i = 0; i < views.GetCount(); i++) {
                 org.sparx.Package currentView = views.GetAt(i);
 
-                
                 System.out.println("View: " + currentView.GetName());
             }
 
-            }
-            catch (Exception e) {
-                System.out.println(rep.GetLastError());   // nice utiility method to print a previous error
-        }
-
+        } catch (Exception e) {
+            System.out.println(e.toString());   
         }
 
     }
+
+}
